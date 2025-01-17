211f1f10a81de8d626f1144990e2b3fdc87745f1
Nicolas Linden <n.linde@naarea.fr>
2025-01-17 15:40:12 +0100

    progressing on packaging

---
+++ plot5d/main.py
@@ -0,0 +1,16 @@
+from plot5d.cli import parser
+from plot5d.app import app
+import socket
+
+def get_port():
+    sock = socket.socket()
+    sock.bind(('', 0))
+    return sock.getsockname()[1]
+
+def main():
+    args = parser.parse_args()
+    if args.port is None:
+        port = get_port()
+    else:
+        port = args.port
+    app.run(debug=True, port=port)
