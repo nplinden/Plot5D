b0f5ea8381b35a7fb58754543dfa6dea3907e51e
Nicolas Linden <n.linde@naarea.fr>
2025-01-22 17:01:01 +0100

    for deployment

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from plot5d.cli import parser
+import os
 from plot5d.app import app
 import socket
 
+
 def get_port():
     sock = socket.socket()
-    sock.bind(('', 0))
+    sock.bind(("", 0))
     return sock.getsockname()[1]
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    if args.port is None:
+    # Vérifier si la variable d'environnement PORTTCP est définie
+    port_env = os.getenv("PORTTCP")
+    if port_env is not None:
+        port = int(port_env)
+    elif args.port is None:
         port = get_port()
     else:
         port = args.port
-    app.run(debug=True, port=port)
+
+    # Lancer l'application
+    app.run(debug=True, port=port, host="0.0.0.0")
