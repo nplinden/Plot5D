0991a666506fb4bdaed4da448989dff91c74f04f
Nicolas Linden <n.linde@naarea.fr>
2025-01-24 10:34:42 +0100

    cleanup

--- plot5d/cli.py
+++
@@ -1,4 +0,0 @@
-import argparse as ap
-
-parser = ap.ArgumentParser(prog="Plot5D", description="A plotting tool for your favorite dataframes")
-parser.add_argument("-p", "--port", help="A port for the debug server", type=int)
